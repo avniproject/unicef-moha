02dfa01187b15150b1f3507255bed87ed44a9394
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-05-07 15:06:12 +0530

    1368 | remove legacy files

--- index.js
+++
@@ -1,6 +0,0 @@
-const {postAllRules} = require("rules-config/infra");
-const _ = require('lodash');
-const serverurl = _.isEmpty(process.argv[2]) ? 'http://localhost:8021': process.argv[2];
-const token = _.isEmpty(process.argv[3]) ? 'DUMMY': process.argv[3];
-const org_admin = _.isEmpty(process.argv[4]) ? 'DUMMY': process.argv[4];
-postAllRules(org_admin, "./rules/index.js", serverurl, token);
